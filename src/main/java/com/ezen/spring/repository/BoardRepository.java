896cdd8291a7a8a32334c34499879f8db5ca4335
alswn3632 <als3632@naver.com>
2025-01-28 22:48:48 +0900

    005 부트스트랩 CSS/JS 적용

--- src/main/java/com/ezen/spring/repository/BoardRepository.java
+++ src/main/java/com/ezen/spring/repository/BoardRepository.java
@@ -4,7 +4,7 @@ import com.ezen.spring.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-
+    // 커스텀 연결 전
 
 
 }
